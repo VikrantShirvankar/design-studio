a81a80f88e669ea46f0acd48a4ef80a1eca27556
Vikrant <vvshirvankar@gmail.com>
2022-02-23 23:15:27 +0530

    Added Navigation component with styling

---
+++ src/Components/Navigatiion/NavItems.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface MenuItem {
+  path: string;
+  name: string;
+}
+
+interface Props {
+  menuItems: MenuItem[];
+}
+
+const NavItems = (props: Props) => {
+  const { menuItems } = props;
+  return (
+    <>
+      {menuItems?.map((menuItem) => (
+        <div className="menu-items">
+          <Link to={menuItem.path}>{menuItem.name}</Link>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default NavItems;
--- src/Components/Navigatiion/Navigation.tsx
+++ src/Components/Navigatiion/Navigation.tsx
@@ -1,15 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import NavItems from './NavItems';
+import './Navigation.scss';
+
+const menuItems = [
+  {
+    name: 'Home',
+    path: '/'
+  },
+  {
+    name: 'About',
+    path: '/about'
+  },
+  {
+    name: 'Portfolio',
+    path: '/portfolio'
+  },
+
+  {
+    name: 'Contact',
+    path: '/contact'
+  }
+];
+
 const Navigation = () => {
+  const [isMobileMenu, setIsMobileMenu] = useState(window.innerWidth <= 768);
+  const [isMneuDrawerOpen, setIsMenuDrawerOpen] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  });
+
+  const handleWindowResize = () => {
+    if (window.innerWidth > 768) {
+      setIsMobileMenu(false);
+      setIsMenuDrawerOpen(false);
+    } else {
+      setIsMobileMenu(true);
+    }
+  };
+
+  const toggleMenuDrawer = () => {
+    setIsMenuDrawerOpen(!isMneuDrawerOpen);
+  };
+
+  const mobileNavigationContainer = isMobileMenu
+    ? 'mobile-navigation-container'
+    : '';
+
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{' '}
-        <Link to="/portfolio">Portfolio</Link> |{' '}
-        <Link to="/contact">Contact</Link> |{' '}
-      </nav>
-      <Outlet />
-    </div>
+    <>
+      <div className={`navigation-container ${mobileNavigationContainer}`}>
+        <div className="menu-title">Rettro</div>
+
+        {!isMobileMenu && (
+          <div className="menu-items-wrapper">
+            <NavItems menuItems={menuItems} />
+          </div>
+        )}
+
+        {isMobileMenu && (
+          <div className="mobile-menu-icon" onClick={toggleMenuDrawer}>
+            <i className="fa fa-bars" aria-hidden="true"></i>
+          </div>
+        )}
+
+        <Outlet />
+      </div>
+      {isMneuDrawerOpen && isMobileMenu && (
+        <div className="mobile-menu-wrapper">
+          <NavItems menuItems={menuItems} />
+        </div>
+      )}
+    </>
   );
 };
 
