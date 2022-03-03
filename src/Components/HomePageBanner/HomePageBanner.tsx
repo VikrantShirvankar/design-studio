9cf0dadeb0cc764e6f18c7b86c217bbe6259121a
Vikrant <vvshirvankar@gmail.com>
2022-03-04 00:02:24 +0530

    Slider image change logic

--- src/Components/HomePageBanner/HomePageBanner.tsx
+++ src/Components/HomePageBanner/HomePageBanner.tsx
@@ -5,66 +5,58 @@ const bannerData = [
   {
     image:
       'http://localhost:3000/static/media/banner1.png.318cddededaeb087df32.webp',
-    thumbnail: '',
+    thumbnail:
+      'https://preview.colorlib.com/theme/rettro/assets/img/hero/slider-dot2.png',
     text: ''
   },
   {
     image:
-      'http://localhost:3000/static/media/banner1.png.318cddededaeb087df32.webp',
-    thumbnail: '',
+      'https://preview.colorlib.com/theme/rettro/assets/img/hero/h1_hero1.png.webp',
+    thumbnail:
+      'https://preview.colorlib.com/theme/rettro/assets/img/hero/slider-dot1.png',
     text: ''
   },
   {
     image:
-      'http://localhost:3000/static/media/banner1.png.318cddededaeb087df32.webp',
-    thumbnail: '',
-    text: ''
-  },
-  {
-    image:
-      'http://localhost:3000/static/media/banner1.png.318cddededaeb087df32.webp',
-    thumbnail: '',
+      'https://preview.colorlib.com/theme/rettro/assets/img/hero/h1_hero3.png.webp',
+    thumbnail:
+      'https://preview.colorlib.com/theme/rettro/assets/img/hero/slider-dot3.png.webp',
     text: ''
   }
 ];
 
 const HomePageBanner = () => {
+  const [bannerActiveImage, setBannerActiveImage] = useState(
+    bannerData?.[0]?.image
+  );
+
+  const changeBannerImage = (requestedImage: string) => {
+    setBannerActiveImage(requestedImage);
+  }
+
   return (
     <div className="banner-container">
       <div
         className="banner-image-container"
         style={{
-          backgroundImage:
-            "url('https://preview.colorlib.com/theme/rettro/assets/img/hero/h1_hero3.png.webp')"
+          backgroundImage: `url(${bannerActiveImage})`
         }}
       >
         <div className="banner-overlay" />
       </div>
-        <div className="banner-content-wrapper">
+      <div className="banner-content-wrapper">
         <div className="banner-content">
           <div className="description">01 Travel Pro</div>
           <div className="thumbnail-wrapper">
-            <div
-              className="thumbnail"
-              style={{
-                backgroundImage:
-                  "url('https://preview.colorlib.com/theme/rettro/assets/img/hero/slider-dot2.png')"
-              }}
-            ></div>
-            <div
-              className="thumbnail"
-              style={{
-                backgroundImage:
-                  "url('https://preview.colorlib.com/theme/rettro/assets/img/hero/slider-dot2.png')"
-              }}
-            ></div>
-            <div
-              className="thumbnail"
-              style={{
-                backgroundImage:
-                  "url('https://preview.colorlib.com/theme/rettro/assets/img/hero/slider-dot2.png')"
-              }}
-            ></div>
+            {bannerData?.map((banner) => (
+              <div
+                className="thumbnail"
+                style={{
+                  backgroundImage: `url(${banner.thumbnail})`
+                }}
+                onClick={() => changeBannerImage(banner.image)}
+              ></div>
+            ))}
           </div>
         </div>
       </div>
